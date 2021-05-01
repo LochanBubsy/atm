af2183c9dae63693b1ea1ab3788eaa781f1b0b53
LochanBubsy <59167606+LochanBubsy@users.noreply.github.com>
2021-05-01 11:40:28 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+class   atm(object):
+    def init (self, gender, age, pin, level):
+
+        self.gender = gender
+        self.age = age
+        self.pin = pin
+        self.level = level
+
+    def gender(self):
+        Gender=int(input("Male or Female"))
+        print (Gender)
+
+    def age(slef):
+        Age=int(input("Your age"))
+        print (Age)
+    
+    def pin(self):
+        Pin=int(input("Your pin(6 Digits)"))
+        print (Pin)
+    
+    
+
