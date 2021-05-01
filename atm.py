0b88b16b430bd179d24a9eaf29c5de384bfd3e2c
LochanBubsy <59167606+LochanBubsy@users.noreply.github.com>
2021-05-01 11:49:21 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -2,12 +2,14 @@ import os
 import shutil
 
 class   atm(object):
-    def init (self, gender, age, pin, level):
+    def init (self, gender, age, pin, level, cashwithdrawal, balanceenquiry):
 
         self.gender = gender
         self.age = age
         self.pin = pin
         self.level = level
+        self.cashwithdrawal = cashwithdrawal
+        self.balanceenquiry = balanceenquiry
 
     def gender(self):
         Gender=int(input("Male or Female"))
@@ -21,5 +23,12 @@ class   atm(object):
         Pin=int(input("Your pin(6 Digits)"))
         print (Pin)
     
+    def cashwithdrawal(self):
+        Cashwithdrawal=int(input("Withdraw your cash"))
+        print (Cashwithdrawal)
+
+    def balanceenquiry(self):
+        Balanceenquiry=int(input("Cash in your account"))
+        print (Balanceenquiry)
     
 atm()
